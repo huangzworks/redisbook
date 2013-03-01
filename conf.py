0c7ea6cde009bca5f3432ae860e92968aa965649
huangz1990 <huangz1990@gmail.com>
2013-03-01 13:07:24 +0800

    添加外链插件，链接到《Redis 命令参考》

--- conf.py
+++ conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax']
+extensions = ['sphinx.ext.mathjax', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -242,3 +242,6 @@ texinfo_documents = [
 #texinfo_show_urls = 'footnote'
 
 highlight_language = 'c'
+
+intersphinx_mapping = { 'redis': ('http://redis.readthedocs.org/en/latest', None) }
+intersphinx_cache_limit = 1
