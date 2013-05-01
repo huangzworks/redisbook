46a2b00de3f1c16fb55bfa7c34d1064bda805267
huangz1990 <huangz1990@gmail.com>
2013-05-01 12:41:54 +0800

    添加生成 PDF 文档所需的设置

--- conf.py
+++ conf.py
@@ -171,20 +171,20 @@ htmlhelp_basename = 'Redisdoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+'pointsize': '11pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+'preamble': '\usepackage{xeCJK}\n\setCJKmainfont{WenQuanYi Micro Hei}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Redis.tex', u'Redis 设计与实现 Documentation',
-   u'huangz1990', 'manual'),
+  ('index', 'Redis.tex', u'Redis 设计与实现',
+   u'黄健宏(huangz1990)', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
